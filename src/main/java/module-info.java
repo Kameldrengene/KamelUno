ea1d2a91e4e2d3a9232b4da73302c31af22d86f5
Frosty <bluedogdk@gmail.com>
2022-01-05 14:23:33 +0100

    First commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.kamelboyz.kameluno {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.kamelboyz.kameluno to javafx.fxml;
+    exports com.kamelboyz.kameluno;
+}

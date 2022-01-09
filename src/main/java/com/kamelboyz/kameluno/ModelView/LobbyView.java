8b8b7ce73329211773f37afb37142999f2c075f3
Frosty <bluedogdk@gmail.com>
2022-01-09 18:43:35 +0100

    Background on Lobby

--- src/main/java/com/kamelboyz/kameluno/ModelView/LobbyView.java
+++ src/main/java/com/kamelboyz/kameluno/ModelView/LobbyView.java
@@ -8,10 +8,18 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.RadialGradient;
+import javafx.scene.paint.Stop;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import lombok.*;
@@ -25,10 +33,14 @@ import java.util.ResourceBundle;
 public class LobbyView {
     private final Pane pane = new Pane();
     private Map<String, Button> buttons = new HashMap<>();
+    private Screen screen = Screen.getPrimary();
+    private Rectangle2D bounds = screen.getVisualBounds();
 
     public LobbyView() {
         buttons.put("test",BootstrapButton.makeBootstrapButton("Test", "btn-danger"));
         pane.getChildren().add(buttons.get("test"));
+        BackgroundFill bgFill = new BackgroundFill(new RadialGradient(0, .01, bounds.getWidth() / 2, bounds.getHeight() / 2, bounds.getWidth() / 2, false, CycleMethod.NO_CYCLE, new Stop(0, Color.rgb(85, 0, 0, 1)), new Stop(1, Color.BLACK)), CornerRadii.EMPTY, Insets.EMPTY);
+        pane.setBackground(new Background(bgFill));
     }
 
     public void initializeButtonClicks(Scene scene, Stage stage) {

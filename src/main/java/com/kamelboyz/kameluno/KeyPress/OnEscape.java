ce769159a2e20d2bdb0bf2803c0fd36ef33eacb1
Frosty <bluedogdk@gmail.com>
2022-01-20 16:52:30 +0100

    Revert "escape promt"
    This reverts commit 8b1e5dcaab87f682a5c5ea45599d1d9b23b54d88.

--- src/main/java/com/kamelboyz/kameluno/KeyPress/OnEscape.java
+++ src/main/java/com/kamelboyz/kameluno/KeyPress/OnEscape.java
@@ -2,12 +2,12 @@ package com.kamelboyz.kameluno.KeyPress;
 
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
-import java.util.Optional;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 public class OnEscape {
     public static void exitOnEsc(Scene scene) {
@@ -15,18 +15,9 @@ public class OnEscape {
             @Override
             public void handle(KeyEvent keyEvent) {
                 if (keyEvent.getCode() == KeyCode.ESCAPE) {
-//                    Stage sb = (Stage) scene.getWindow();
-//                    sb.close();
-//                    System.exit(0);
-                    Alert nalert = new Alert(Alert.AlertType.NONE,"Exit Game ?", ButtonType.YES, ButtonType.NO);
-                    nalert.setTitle("Kamel Uno");
-                    Optional<ButtonType> option = nalert.showAndWait();
-
-                    if(option.get()==ButtonType.YES){
-                        Stage sb = (Stage) scene.getWindow();
-                        sb.close();
-                        System.exit(0);
-                    }
+                    Stage sb = (Stage) scene.getWindow();
+                    sb.close();
+                    System.exit(0);
                 }
             }
         });
